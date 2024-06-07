bd2c49a7413144a10b3aceda2fa6ba4995520c10
Mabuza713 <uxbeix@gmail.com>
2024-06-07 10:35:18 +0200

    simple stock price predictor using Support Vector Regression

---
+++ Stock.py
@@ -0,0 +1,58 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.svm import SVR
+import yfinance as yf
+import pandas as pd
+
+
+
+
+
+
+class StockPredictor:
+    def __init__(self, stockName):
+        self.stockName = stockName
+
+
+
+  
+    def GetData(self):
+        dates = [0]
+        prices = []
+        
+        data = yf.download(self.stockName, period= "3mo")
+        for index, rows in data.iterrows():
+            
+            prices.append(rows.array[0])
+            
+        dates = range(0,len(prices))
+        return [prices, dates]
+    
+    def PredictPrices(self, x):
+        data = self.GetData()
+        prices = data[0]; dates = data[1]
+        dates = np.reshape(dates, (len(dates),1))
+        
+        svrLin = SVR(kernel= "linear", C=1e3)
+        svrPoly = SVR(kernel= "poly", C=1e3, degree = 2)
+        svrRbf = SVR(kernel = "rbf", C=1e3, gamma = 0.1)
+        
+        svrLin.fit(dates, prices); svrPoly.fit(dates, prices)
+        svrRbf.fit(dates, prices)
+        
+        plt.scatter(dates, prices, color="black", label = "Data")
+        plt.plot(dates, svrRbf.predict(dates), color="blue", label = "Radial Basis Function - RBF")
+        plt.plot(dates, svrLin.predict(dates), color="red", label = "Linear model")
+        plt.plot(dates, svrPoly.predict(dates), color="green", label = "Polynomial model")        
+        
+        plt.xlabel("Date")
+        plt.ylabel("Prices")
+        plt.title("Support Vector Regression - SVR")
+        plt.legend()
+        plt.show()
+        
+        return svrRbf.predict(x)[0], svrLin.predict(x)[0], svrPoly.predict(x)[0]
+
+result =  StockPredictor("AAPL")
+result.PredictPrices(9)
